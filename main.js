bc0c9064c2d1fad9fa707d3354d45d210391c2d6
MeznahAldossari <MeznahAldossari@hotmail.com>
2024-05-21 20:54:53 +0300

    v2

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// ============================ Lab 1==================================
 //1
 let widht = 20;
 let height = 10;
@@ -45,3 +46,80 @@ var interest = "cats";
 var hobby = "play video games";
 var awesomeMessage = `Hi, my name is ${firstName}. I love ${interest}. In my spare time, I like to ${hobby}.`;
 console.log(awesomeMessage);
+
+// ============================ Lab 2 ==================================
+console.log(
+  "============================ Lab 2 =================================="
+);
+
+//1
+
+let eatsPlants = false;
+let eatsAnimals = true;
+
+let animal =
+  eatsPlants && eatsAnimals
+    ? "omnivore"
+    : eatsPlants
+    ? "herbivore"
+    : eatsAnimals
+    ? "carnivore"
+    : undefined;
+
+console.log(`(${eatsPlants} , ${eatsAnimals}, ${animal})`);
+
+//2
+let musicians = 1;
+
+if (musicians <= 0) {
+  console.log("not a group");
+} else if (musicians == 1) {
+  console.log("solo");
+} else if (musicians == 2) {
+  console.log("duet");
+} else if (musicians == 3) {
+  console.log("duet");
+} else if (musicians == 4) {
+  console.log("trio");
+} else {
+  console.log("this is a large group");
+}
+
+//4
+let balance = 3231;
+let checkBalance = true;
+let isActive = true;
+if (checkBalance && isActive && balance < 5) {
+  console.log("the balance has been checked");
+} else if (checkBalance && !isActive && balance > 5) {
+  console.log("the balance has been checked");
+} else if (checkBalance && isActive && balance > 5) {
+  console.log("the balance has been checked");
+}
+
+//5
+let flavor = true;
+let vessel = true;
+let toppings = true;
+
+if (
+  flavor == "vanilla" ||
+  (flavor == "chocolate" && vessel == "cone") ||
+  (vessel == "bowl" && toppings == "sprinkles") ||
+  toppings == "peanuts"
+) {
+  console.log(
+    "I'd like two scoops of __________ ice cream in a __________ with __________."
+  );
+}
+
+//3 ????
+
+var room = "billiards room";
+
+var suspect = "Mr. Parkes";
+
+var weapon = room;
+var solved = false;
+
+//
